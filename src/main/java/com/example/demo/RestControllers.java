52fd36cb5dd6b6bf2c7c6a20ba99f73e0d04c518
anupws@gmail.com <anupws@gmail.com>
2024-04-29 12:11:06 +0530

    first commit

---
+++ src/main/java/com/example/demo/RestControllers.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RestControllers {
+
+     @GetMapping("/set")	
+	public ResponseEntity<String> get(){
+		
+		String msg ="welcome to git";
+		return new ResponseEntity<String>(msg,HttpStatus.OK);
+	}
+	
+}
